e9bb39fdb5f508209efb48b7ff4cdf98c8c2ac61
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-01-21 15:29:18 +0200

    Initial commit

---
+++ Module1Work4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module1Work4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
