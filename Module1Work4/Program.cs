6541bd6f09f297f04a3b2ab4bb761b79ed34c981
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-01-21 15:45:11 +0200

    Added StyleCop settings.ruleset

--- Module1Work4/Program.cs
+++ Module1Work4/Program.cs
@@ -2,11 +2,10 @@
 
 namespace Module1Work4
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
