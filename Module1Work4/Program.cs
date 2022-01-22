d9d3ef87b47cfea162b5adda6ee5d9da85893968
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-01-22 16:34:29 +0200

    Add func to Distribution And Encoding arrays, Search Chars and Print result

--- Module1Work4/Program.cs
+++ Module1Work4/Program.cs
@@ -8,19 +8,22 @@ namespace Module1Work4
         {
             int numberOfElements = TakeNumberOfElements();
 
-            int[] firstArray = new int[numberOfElements];
+            string[] generalArray = new string[numberOfElements];
             int evenCount = 0;
+
             var rand = new Random();
 
             for (int i = 0; i < numberOfElements; i++)
             {
-                firstArray[i] = rand.Next(97, 122);
+                generalArray[i] = rand.Next(97, 122).ToString();
 
-                if (firstArray[i] % 2 == 0)
+                if (int.Parse(generalArray[i]) % 2 == 0)
                 {
                     evenCount++;
                 }
             }
+
+            DistributionAndEncoding(generalArray, evenCount, numberOfElements);
         }
 
         public static int TakeNumberOfElements()
@@ -41,8 +44,88 @@ namespace Module1Work4
             return numberOfElements;
         }
 
-        private void ArrayDivision()
+        private static void DistributionAndEncoding(string[] generalArray, int evenCount, int numberOfElements)
         {
+            int unevenCount = numberOfElements - evenCount;
+
+            string[] arrayOfEven = new string[evenCount];
+            string[] arrayOfUneven = new string[unevenCount];
+
+            int e = 0;
+            int u = 0;
+            int countE = 0;
+            int countU = 0;
+
+            for (int i = 0; i < generalArray.Length; i++)
+            {
+                int element = int.Parse(generalArray[i]);
+
+                if (element % 2 == 0)
+                {
+                    arrayOfEven[e] = Convert.ToChar(element).ToString();
+
+                    if (SearchChar(arrayOfEven[e].ToString()))
+                    {
+                        arrayOfEven[e] = arrayOfEven[e].ToUpper();
+                        countE++;
+                    }
+
+                    e++;
+                }
+                else
+                {
+                    arrayOfUneven[u] = Convert.ToChar(element).ToString();
+
+                    if (SearchChar(arrayOfUneven[u].ToString()))
+                    {
+                        arrayOfUneven[u] = arrayOfUneven[u].ToUpper();
+                        countU++;
+                    }
+
+                    u++;
+                }
+            }
+
+            PrintResults(arrayOfEven, arrayOfUneven, countE, countU);
+        }
+
+        private static bool SearchChar(string str)
+        {
+            string[] strArray = { "a", "e", "i", "d", "h", "j" };
+
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                if (str == strArray[i])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static void PrintResults(string[] arrayOfEven, string[] arrayOfUneven, int countE, int countU)
+        {
+            string result = countE > countU ? $"Uppercase letters more in the first array - ({countE}/{countU})" :
+                $"Uppercase letters more in the second array - ({countE}/{countU})";
+
+            Console.WriteLine(result);
+
+            Console.WriteLine("1. Array of even:");
+            for (int i = 0; i < arrayOfEven.Length; i++)
+            {
+                Console.Write($"{arrayOfEven[i]} ");
+            }
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("2. Array of uneven:");
+            for (int i = 0; i < arrayOfUneven.Length; i++)
+            {
+                Console.Write($"{arrayOfUneven[i]} ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
